edf9038956bf88f8f2da7490c9fdc3ebc9014f8c
Murillo Souza <murillo.souza@evolucao-itanhaem.com>
2022-08-27 09:20:04 -0300

    exercício de programação

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Preparar...");
+Thread.Sleep(1500);
+Console.WriteLine("Apontar...");
+Thread.Sleep(1500);
+Console.WriteLine("Fogo!");
+Thread.Sleep(1500);
+Console.WriteLine("Pressione uma tecla para continuar...");
+Console.ReadKey();
+
+
+
+
